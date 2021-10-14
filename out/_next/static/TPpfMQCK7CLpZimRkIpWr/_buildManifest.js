c0b6137e09ed2f712a4220bba4f028454d6c84a1
Peter Van Dyken <pvandyk2@uwo.ca>
2021-10-13 23:09:41 -0400

    Fixed button alignment

---
+++ out/_next/static/TPpfMQCK7CLpZimRkIpWr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2c8f2cbda4c8cd4a1d09.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
