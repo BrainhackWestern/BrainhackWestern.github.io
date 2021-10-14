4e474ceb66d97756ab0ce01fcc8606c853fd007d
Peter Van Dyken <pvandyk2@uwo.ca>
2021-10-13 23:03:53 -0400

    Deploy Next.js to gh-pages

---
+++ out/_next/static/uSnAmo-sHW1H4Capt8bfB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-353b3190694f23e0263f.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
