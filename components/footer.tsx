cf863c7dd00eb60e3bbcf006ec456c23bb7f84ff
Peter Van Dyken <pvandyk2@uwo.ca>
2022-10-03 11:53:20 -0400

    Update twitter feed

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,12 @@
 import Obfuscate from "react-obfuscate";
 
-import { ExpandedConfig, SiteConfig } from "../interfaces/site-config"
+import { ExpandedConfig, SiteConfig } from "../interfaces/site-config";
 
 interface FooterProps {
-  organizers: string[]
-  twitterUrl?: string
-  displayTwitter: boolean
-  year: number
+  organizers: string[];
+  twitterUrl?: string;
+  displayTwitter: boolean;
+  year: number;
 }
 
 export const getFooterProps = (config: ExpandedConfig) => {
@@ -15,62 +15,44 @@ export const getFooterProps = (config: ExpandedConfig) => {
     twitterUrl: config.twitterUrl,
     displayTwitter: config.displaySections.twitterFeed ?? true,
     year: config.currentYear,
-
-  }
-}
+  };
+};
 
 export const Footer = (props: FooterProps) => {
   return (
-          <footer>
-        <div className="content-space container-lg">
-          <div className="row">
-            <div className="col-lg-6 d-flex flex-column justify-content-start align-items-start">
-              <h3>Organizers</h3>
-              <p className="organizers">{props.organizers.join(", ")}</p>
-            </div>
-            {props.displayTwitter && props.twitterUrl ? (
-              <div className="col-lg-6 d-flex flex-column justify-content-between align-items-center">
-                <p>
-                  <a
-                    href={props.twitterUrl}
-                    className="twitter-follow-button"
-                    data-dnt="true"
-                    data-show-count="false"
-                  >
-                    Follow brainhackUWO
-                  </a>
-                  <script
-                    async
-                    src="https://platform.twitter.com/widgets.js"
-                    charSet="utf-8"
-                  ></script>
-                  <br />
-                  <a
-                    className="twitter-timeline"
-                    data-width="400"
-                    data-height="500"
-                    data-dnt="true"
-                    data-theme="dark"
-                    href="https://twitter.com/brainhackUWO?ref_src=twsrc%5Etfw"
-                  >
-                    Tweets by brainhackUWO
-                  </a>{" "}
-                  <script
-                    async
-                    src="https://platform.twitter.com/widgets.js"
-                    charSet="utf-8"
-                  ></script>
-                </p>
-              </div>
-            ) : null}
+    <footer>
+      <div className="content-space container-lg">
+        <div className="row">
+          <div className="col-lg-6 d-flex flex-column justify-content-start align-items-start">
+            <h3>Organizers</h3>
+            <p className="organizers">{props.organizers.join(", ")}</p>
           </div>
-          <h3 id="contact">Contact</h3>
-          <Obfuscate email="brainhack.western@gmail.com" />
-          <p className="copyright">Copyright © {props.year} Brainhack Western</p>
+          {props.displayTwitter && props.twitterUrl ? (
+            <div className="col-lg-6 d-flex flex-column justify-content-between align-items-center">
+              <p>
+                <a
+                  className="twitter-timeline"
+                  data-dnt="true"
+                  data-theme="light"
+                  href={props.twitterUrl}
+                >
+                  Tweets by UWOBrainhack
+                </a>{" "}
+                <script
+                  async
+                  src="https://platform.twitter.com/widgets.js"
+                  charSet="utf-8"
+                ></script>
+              </p>
+            </div>
+          ) : null}
         </div>
-      </footer>
-
-  )
-}
+        <h3 id="contact">Contact</h3>
+        <Obfuscate email="brainhack.western@gmail.com" />
+        <p className="copyright">Copyright © {props.year} Brainhack Western</p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
