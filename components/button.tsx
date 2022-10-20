a57aedfc4839e9450c50c138f13e614ae4ed1831
Peter Van Dyken <pvandyk2@uwo.ca>
2022-10-20 16:49:01 -0400

    Fix button to use Next Link

--- components/button.tsx
+++ components/button.tsx
@@ -1,18 +1,28 @@
+import Link from 'next/link';
+
 interface ButtonProps {
   children: React.ReactNode;
   target?: string;
   className?: string;
 }
 
+const LinkIfTarget = ({
+  target,
+  children
+}: {
+  target?: string;
+  children: React.ReactChild;
+}) => {
+  return target ? <Link href={target}>{children}</Link> : <>{children}</>;
+};
 export const Button = (props: ButtonProps) => {
   return (
-    <a
-      className={['button', props.className ?? ''].join(' ')}
-      href={props.target}
-    >
-      <div className="inner">
-        <span>{props.children}</span>
-      </div>
-    </a>
+    <LinkIfTarget target={props.target}>
+      <a className={['button', props.className ?? ''].join(' ')}>
+        <div className="inner">
+          <span>{props.children}</span>
+        </div>
+      </a>
+    </LinkIfTarget>
   );
 };
