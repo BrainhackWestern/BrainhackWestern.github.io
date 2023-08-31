7b17f2ab642892ee39e339ffa08b5e72d58cda36
Peter Van Dyken <pvandyk2@uwo.ca>
2023-08-31 12:26:51 -0400

    Add missing Image component

---
+++ src/components/image.tsx
@@ -0,0 +1,15 @@
+import NextImage, { ImageLoader, ImageProps } from 'next/image';
+import { isString } from 'lodash';
+
+const basePath = process.env.NEXT_PUBLIC_URL;
+
+const Image = (props: ImageProps) => {
+  return (
+    <NextImage
+      {...props}
+      src={isString(props.src) && basePath ? basePath + props.src : props.src}
+    />
+  );
+};
+
+export default Image;
