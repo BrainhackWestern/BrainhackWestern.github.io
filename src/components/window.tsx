bd047fbf1aaa6ec93bfe4a478eff8958350e7d8b
Peter Van Dyken <pvandyk2@uwo.ca>
2023-09-05 14:37:19 -0400

    Minor fixes

--- src/components/window.tsx
+++ src/components/window.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 import { window } from './window.css';
 
-const Window = ({ children, half }: PropsWithChildren<{ half?: boolean}>) => (
+const Window = ({ children, half }: PropsWithChildren<{ half?: boolean }>) => (
   <div className={window} style={{ height: half ? '50vh' : '100vh' }}>
     {children}
   </div>
